85a00e1b486d62ef474bfd67fe9b8f526524bc40
irinedz <irinezonson@gmail.com>
2022-09-19 10:27:49 +0530

    save

---
+++ travel2project/travelapp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import place
+# Create your views here.
+
+def demo(request):
+    obj=place.objects.all
+    return  render(request,"index.html",{'result':obj})
